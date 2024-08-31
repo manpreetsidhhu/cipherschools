2588ea30fff18aa47c2f8648f92196e21e9cddd8
Manpreet Singh Sidhu <preetsidhu2549@gmail.com>
2024-08-31 09:01:56 +0530

    Day 7

---
+++ day 7.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n, choice;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Choose sorting algorithm:\n1. Bubble Sort\n2. Insertion Sort\nEnter your choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        bubbleSort(arr, n);
+    } else if (choice == 2) {
+        insertionSort(arr, n);
+    } else {
+        cout << "Invalid choice" << endl;
+        return 1;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
